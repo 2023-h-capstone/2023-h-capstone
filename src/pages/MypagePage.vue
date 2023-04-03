83b738bab5e7659508ed70226b781b79f707f3da
201810807 <101114300+201810807@users.noreply.github.com>
2023-04-04 03:07:39 +0900

    FEAT : Mypage 화면구성

--- src/pages/MypagePage.vue
+++ src/pages/MypagePage.vue
@@ -1,45 +1,57 @@
 <template>
-<q-layout style="text-align: center; padding-top: 5vh; padding-left:25rem;">
-  <q-avatar size="250px" style="text-align: center;">
-      <img src='../assets/profileTest.jpg'>
+<q-layout style="text-align: center; padding-top: 1vh; padding-left:25rem;">
+  <q-item style="text-align: center;">
+    <q-avatar clickable v-ripple size="250px" style="text-align: center;">
+        <img src='../assets/profileTest.jpg'>
     </q-avatar>
-    <div style="font-size: 2em; font-weight: bolder;padding-bottom: 2em;">사용자 닉네임1</div>
+      
+      <q-item-section style="text-align: left; font-size: 2em; font-weight: bolder;padding-bottom: 2em;">my_account_hello</q-item-section>
+  
 
-    <q-item clickable v-ripple>
-        <q-item-section avatar>
-          <q-icon color="primary" name="bluetooth" />
+  <div style="padding-right: 16vh; padding-top: 14vh;" class="q-pa-md">
+    <div class="q-gutter-y-md column">
+      
+      <q-toolbar class="text-black rounded-borders">
+
+        <q-separator dark vertical inset />
+
+        <q-btn style="font-weight: bolder;" stretch flat label="내 친구" />
+
+        <q-separator dark vertical />
+
+        <q-btn style="font-weight: bolder;" stretch flat label="새 게시글 작성" />
+
+        <q-separator dark vertical />
+
+        <q-btn style="font-weight: bolder;" stretch flat label="내 테마 모음" />
+
+        <q-separator dark vertical />
+
+        <q-btn style="font-weight: bolder;" stretch flat label="내 즐겨찾기 모음" />
+      </q-toolbar>
+    </div>
+  </div>
+</q-item>
+    
+
+<q-layout style="text-align: right; ">
+    <q-list bordered>
+      <q-item v-for="(place, key) in places" v-bind:key="key" class="q-my-sm" clickable v-ripple>
+        <q-item-section>
+          <q-item-label style="font-size: 20px;">{{ place.name }}</q-item-label>
+          <q-item-label style="font-size: 15px;" caption lines="1">
+              <span v-for="(tag, key) in place.tags" v-bind:key="key">
+                {{tag}}
+              </span>
+          </q-item-label>
         </q-item-section>
-
-        <q-item-section>Icon as avatar(여기부터는)</q-item-section>
-      </q-item>
-
-      <q-item clickable v-ripple>
-        <q-item-section avatar>
-          <q-avatar color="teal" text-color="white" icon="bluetooth" />
+        <q-item-section side>
+          <q-radio v-model="shape" :val="key"/>
         </q-item-section>
-
-        <q-item-section>Avatar-type icon(테스트)</q-item-section>
       </q-item>
-
-      <q-item clickable v-ripple>
-        <q-item-section avatar>
-          <q-avatar rounded color="purple" text-color="white" icon="bluetooth" />
-        </q-item-section>
-
-        <q-item-section>Rounded avatar-type icon(내용입니다)</q-item-section>
-      </q-item>
-
-      <q-item clickable v-ripple>
-        <q-item-section avatar>
-          <q-avatar color="primary" text-color="white">
-            R
-          </q-avatar>
-        </q-item-section>
-
-        <q-item-section>Letter avatar-type(진행 중)</q-item-section>
-      </q-item>
-
-      <q-separator />
+      <q-separator/>
+    </q-list>
+  </q-layout>
 
 
 </q-layout>
@@ -48,24 +60,55 @@
   <script lang="ts">
   import { defineComponent, ref } from 'vue';
   import {useQuasar} from 'quasar';
+  import {myPlaces} from 'assets/ContentModel';
+
   
   
   export default defineComponent({
     name: 'MypagePage',
-    setup () {
-      return {
+    components: { },
+  setup () {
+    return {
+      shape: ref(null),
+      fixed: ref(false),
+    }
+  },
+  data() {
+    return {
+      contentStyle: {
+        width: '100%',
+        height: '100vh',
+        display:'block',
+      },
+      places: ref<[{
+        id: number,
+        name: string,
+        tags: [],
+      }] | any>(),
+      title: ref<string>(''),
+      tag: ref<string>(''),
+      tags: ref<string[]>([])
+    }
+  },
+  async mounted() {
+    this.places = [...myPlaces]
+    this.$q.loading.show()
+    this.$q.loading.hide()
+
+  },
+  methods: {
+    addTag: function () {
+      if(this.tags.length == 0) {
+        return
       }
+      this.places.push({
+        id: 0,
+        name: this.title,
+        tags: this.tags
+      })
+      this.tags = []
+      this.title = ''
     },
-    data() {
-      return {
-        //
-      }
-    },
-    mounted() {
-      //
-    },
-    methods: {
-      //
-    },
-  });
+  },
+});
   </script>
