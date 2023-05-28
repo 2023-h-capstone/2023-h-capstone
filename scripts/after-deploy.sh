f464235bf0e4cf07d6ff6b831ed6be19fbc4ace7
곰개구리 <66404645+bear-frog@users.noreply.github.com>
2023-05-28 15:15:50 +0900

    Update after-deploy.sh

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -2,7 +2,7 @@
 REPOSITORY=/home/ubuntu/app/dist/ssr
 
 cd $REPOSITORY
-# sudo npm i
+npm i
 # 참조
 # https://unchae.tistory.com/entry/PM2-80-443%ED%8F%AC%ED%8A%B8-%EC%82%AC%EC%9A%A9
-pm2 start index.js --watch
+pm2 reload index.js --watch
